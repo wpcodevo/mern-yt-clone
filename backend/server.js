cf6abb3e114f1a00f1111104fe8561e91fd8b103
Edem <ziddah.edem@gmail.com>
2021-08-07 14:34:02 +0000

    vercel commit

--- backend/server.js
+++ backend/server.js
@@ -1,65 +1,74 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import productRouter from './routes/productRoutes.js'
-import userRouter from './routes/userRoutes.js'
-import orderRouter from './routes/orderRoutes.js'
-import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
-process.on('uncaughtException', err => {
-  console.log(err.name, err.message)
-  process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
+app.use(cors());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // MiddleWares
-app.use(express.json())
+app.use(express.json());
 
 // Database
-connectDB()
+connectDB();
 
 // Routes
-app.use('/api/products', productRouter)
-app.use('/api/users', userRouter)
-app.use('/api/orders', orderRouter)
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
-app.get('/api/config/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID)
-})
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
-const __dirname = path.resolve()
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+// const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(process.cwd(), "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.join(process.cwd(), "/frontend/build/index.html"),
+      (err) => {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    )
+  );
 }
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
     `server started successfully in ${process.env.NODE_ENV} on port: ${PORT}`
       .underline.bold.yellow
   )
-)
+);
 
-process.on('unhandledRejection', () => {
-  console.log('UNHANDLED REJECTION Shutting down...'.underline.bold.red)
+process.on("unhandledRejection", () => {
+  console.log("UNHANDLED REJECTION Shutting down...".underline.bold.red);
   server.close(() => {
-    process.exit(1)
-  })
-})
+    process.exit(1);
+  });
+});
